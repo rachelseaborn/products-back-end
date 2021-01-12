a12a71c4d5867d49dcc1173533e71f9ee3f0e9e6
rachelseaborn <raseaborn@gmail.com>
2021-01-11 18:36:59 -0700

    all postman tests passed

---
+++ server/index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const express = require('express');
+const massive = require('massive');
+const products_controller = require('./products_controller')
+
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+
+const app = express();
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: { rejectUnauthorized: false }
+}).then(dbInstance => {
+    app.set('db', dbInstance);
+    console.log('db connected');
+})
+    .catch(err => console.log(err));
+
+app.use(express.json());
+
+app.get('/api/products', products_controller.getAll);
+
+app.get('/api/products/:id', products_controller.getOne);
+
+app.put('/api/products/:id', products_controller.update);
+
+app.post('/api/products', products_controller.create);
+
+app.delete('/api/products/:id', products_controller.delete);
+
+
+
+app.listen(SERVER_PORT, () => console.log(`Server is running on ${SERVER_PORT}`));
